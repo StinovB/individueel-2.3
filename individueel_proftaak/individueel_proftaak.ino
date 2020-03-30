8e2ebe8c8ba334f70c7f36b88d97a35705d5fd48
StinovB <sj.vanberkel@student.avans.nl>
2020-03-30 20:09:47 +0200

    file created

---
+++ individueel_proftaak/individueel_proftaak.ino
@@ -0,0 +1,16 @@
+int temperatureSensor = 5;
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  //pinMode(temperatureSensor, INPUT);
+
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly
+  if(digitalRead(output1)== HIGH){
+
+}
