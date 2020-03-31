bbacaddbb36d1ec79c3897bf30de7e1c3e11cc47
StinovB <sj.vanberkel@student.avans.nl>
2020-03-31 10:42:11 +0200

    abillity to read temperature

--- individueel_proftaak/individueel_proftaak.ino
+++ individueel_proftaak/individueel_proftaak.ino
@@ -1,16 +1,23 @@
-int temperatureSensor = 5;
+#include <OneWire.h>
+#include <DallasTemperature.h> 
 
+float temp = 0.0;
+int oneWireBus = 12;
+OneWire oneWire(oneWireBus);
+DallasTemperature sensors(&oneWire);
 
 
 void setup() {
-  // put your setup code here, to run once:
-  //pinMode(temperatureSensor, INPUT);
-
-  
+    Serial.begin(9600);
+    Serial.println("Stijn van Berkel indidueel");
+    sensors.begin();
+   
 }
 
 void loop() {
-  // put your main code here, to run repeatedly
-  if(digitalRead(output1)== HIGH){
-
+    sensors.requestTemperatures();
+    temp = sensors.getTempCByIndex(0);
+    Serial.print("temperature is: ");
+    Serial.println(temp);
+    delay(1000);
 }
